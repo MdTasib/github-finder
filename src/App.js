8f482734c8d5f74e7119da74068063ba45ce88c6
MdTasib <ohidulalam442@gmail.com>
2022-06-26 22:47:15 +0600

    create github context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import { GithubProvider } from "./context/github/GithubContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
 	return (
-		<div className='flex flex-col justify-between h-screen'>
-			<Navbar />
-			<main className='container mx-auto'>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/home' element={<Home />} />
-					<Route path='/about' element={<About />} />
-					<Route path='*' element={<NotFound />} />
-				</Routes>
-			</main>
-			<Footer />
-		</div>
+		<GithubProvider>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+				<main className='container mx-auto'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/home' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</GithubProvider>
 	);
 }
 

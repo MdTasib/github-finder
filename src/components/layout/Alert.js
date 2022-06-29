2fe8ef767279198750e02c31779d4964f94b1927
MdTasib <ohidulalam442@gmail.com>
2022-06-29 12:02:46 +0600

    error fix

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -15,20 +15,20 @@ function Alert() {
 			className={`grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 pt-5`}
 			style={{ visibility: alert ? "visible" : "hidden" }}>
 			<div
-				class='flex p-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'
+				className='flex p-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'
 				role='alert'>
 				<svg
-					class='inline flex-shrink-0 mr-3 w-5 h-5'
+					className='inline flex-shrink-0 mr-3 w-5 h-5'
 					fill='currentColor'
 					viewBox='0 0 20 20'
 					xmlns='http://www.w3.org/2000/svg'>
 					<path
-						fill-rule='evenodd'
+						fillRule='evenodd'
 						d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
-						clip-rule='evenodd'></path>
+						clipRule='evenodd'></path>
 				</svg>
 				<div>
-					<span class='font-medium'>{alert?.msg}</span>
+					<span className='font-medium'>{alert?.msg}</span>
 				</div>
 			</div>
 		</div>
